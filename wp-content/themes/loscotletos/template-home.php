28ab5c65589975793ee89440ea6e6b209f6dc112
Ilya13 <shibaev.ilya@4px.ru>
2020-11-16 13:14:12 +0300

    bug fix

--- wp-content/themes/loscotletos/template-home.php
+++ wp-content/themes/loscotletos/template-home.php
@@ -9,32 +9,31 @@ $email = $redux_loscotletos['lc-email'];
 get_header();
 ?>
     <main>
-        <div class="main-slider">
+        <div class="main-slider" style="margin-bottom: 50px;">
             <div class="js-main-slider">
                 <div class="main-slider__item">
-                    <img data-lazy="<?php echo get_template_directory_uri() ?>img/slider/1.jpg"
+                    <img data-lazy="<?php echo get_template_directory_uri() ?>/img/slider/1.jpg"
                          src="<?php echo get_template_directory_uri() ?>/img/slider/1.jpg" alt="loscotletos">
                 </div>
                 <div class="main-slider__item">
-                    <img data-lazy="<?php echo get_template_directory_uri() ?>img/slider/1.jpg"
+                    <img data-lazy="<?php echo get_template_directory_uri() ?>/img/slider/1.jpg"
                          src="<?php echo get_template_directory_uri() ?>/img/slider/2.jpg" alt="loscotletos">
                 </div>
                 <div class="main-slider__item">
-                    <img data-lazy="<?php echo get_template_directory_uri() ?>img/slider/1.jpg"
+                    <img data-lazy="<?php echo get_template_directory_uri() ?>/img/slider/1.jpg"
                          src="<?php echo get_template_directory_uri() ?>/img/slider/3.jpg" alt="loscotletos">
                 </div>
                 <div class="main-slider__item">
-                    <img data-lazy="<?php echo get_template_directory_uri() ?>img/slider/1.jpg"
+                    <img data-lazy="<?php echo get_template_directory_uri() ?>/img/slider/1.jpg"
                          src="<?php echo get_template_directory_uri() ?>/img/slider/4.jpg" alt="loscotletos">
                 </div>
                 <div class="main-slider__item">
-                    <img data-lazy="<?php echo get_template_directory_uri() ?>img/slider/1.jpg"
+                    <img data-lazy="<?php echo get_template_directory_uri() ?>/img/slider/1.jpg"
                          src="<?php echo get_template_directory_uri() ?>/img/slider/5.jpg" alt="loscotletos">
                 </div>
             </div>
         </div>
         <div>
-            <h3>Это index.php</h3>
             <div class="main-page__wrapper">
                 <section class="main_column">
                     <?php
@@ -83,7 +82,7 @@ get_header();
                                     ?>
                                 </div>
                                 <div class="preview_card__text__buttons">
-                                    <a href="<?php ' . esc_url(get_permalink()) . '?>" rel="bookmark">
+                                    <a href="<?php echo esc_url(get_permalink()) ?>" rel="bookmark">
                                         Read more <img class="arrow" src="<?php echo get_template_directory_uri() ?>/img/style/arrow.svg" width="15" height="10" alt="">
                                     </a>
 
